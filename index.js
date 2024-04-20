474f54133334ea8da593d71f01f94ad1d487e704
uttam kamar <kamaruttam@gmail.com>
2024-04-20 11:33:34 +0530

    api live

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+const port = process.env.PORT || 3000;
+const apiData = require("./data.json");
+
+app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/service", (req, res) => res.send(apiData));
+app.listen(port, () => console.log(`App listening on port ${port}!`));
